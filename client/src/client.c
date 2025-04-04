8652708441e3b6a214a3082d4228031a68bcfac4
Ramiro Iozzo <riozzo@frba.utn.edu.ar>
2025-04-04 00:09:23 -0300

    done

--- client/src/client.c
+++ client/src/client.c
@@ -15,20 +15,22 @@ int main(void)
 	/* ---------------- LOGGING ---------------- */
 
 	logger = iniciar_logger();
-
+	
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
-
+	log_info(logger, "Hola! Soy un tronco");
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
+	
 	config = iniciar_config();
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
-
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 	// Loggeamos el valor de config
-
+	log_info(logger, valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -42,7 +44,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-
+	enviar_mensaje(valor, conexion);
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
@@ -50,19 +52,26 @@ int main(void)
 
 	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
 	// Proximamente
+	//aprovecho pa barrer
+	//suban los pies
+	log_destroy(logger);
+	config_destroy(config);
+	//pueden bajarlos
 }
 
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
-
+	nuevo_logger = log_create("tp0.log", "Client.c", true, LOG_LEVEL_INFO);
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+	char *path= getcwd(NULL, 0);
+	string_append(&path, "/cliente.config");
+	nuevo_config = config_create(path);
 	return nuevo_config;
 }
 
@@ -72,12 +81,17 @@ void leer_consola(t_log* logger)
 
 	// La primera te la dejo de yapa
 	leido = readline("> ");
-
+	
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
-
+	while (strcmp("", leido)!=0)
+	{
+		log_info(logger, leido);
+		leido = readline("> ");
+	}
+	
+	
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -87,10 +101,24 @@ void paquete(int conexion)
 	t_paquete* paquete;
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
-
-	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	paquete = crear_paquete();
+	while (true)
+	{
+		leido = readline("(·)(·)> ");
+		printf("bro said");
+		printf(leido);
+		agregar_a_paquete(paquete, leido, sizeof(leido)+1);
+		if (strcmp(leido, "")==0)
+		{
+			break;
+		}
+	}
+	enviar_paquete(paquete, conexion);
 	
+	
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	free(leido);
+	free(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
--- server/src/utils.c
+++ server/src/utils.c
@@ -5,7 +5,7 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	//assert(!"no implementado!");
 
 	int socket_servidor;
 
@@ -19,10 +19,12 @@ int iniciar_servidor(void)
 	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
 	// Creamos el socket de escucha del servidor
-
+	socket_servidor = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 	// Asociamos el socket a un puerto
-
+	setsockopt(socket_servidor, SOL_SOCKET,SO_REUSEPORT, &(int){1}, sizeof(int));//?
+	bind(socket_servidor,servinfo->ai_addr, servinfo->ai_addrlen);
 	// Escuchamos las conexiones entrantes
+	listen(socket_servidor, SOMAXCONN);
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -33,10 +35,10 @@ int iniciar_servidor(void)
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	//?assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente =accept(socket_servidor, NULL, NULL);
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
@@ -89,7 +91,7 @@ t_list* recibir_paquete(int socket_cliente)
 		char* valor = malloc(tamanio);
 		memcpy(valor, buffer+desplazamiento, tamanio);
 		desplazamiento+=tamanio;
-		list_add(valores, valor);
+		list_add(valores, valor);	
 	}
 	free(buffer);
 	return valores;
